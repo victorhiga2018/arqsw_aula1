99ee4de7649608b566ec63c599cce5c7b548e69d
victor <viih.higa@gmail.com>
2018-04-02 21:02:35 -0300

    entity

---
+++ arqsw_aula1/src/br/usjt/arqsw/service/ChamadoService.java
@@ -0,0 +1,36 @@
+package br.usjt.arqsw.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import br.usjt.arqsw.dao.ChamadoDAO;
+import br.usjt.arqsw.entity.Chamado;
+import br.usjt.arqsw.entity.Fila;
+
+/**
+ * 
+ * @author victor RA:201513337
+ *
+ */
+
+public class ChamadoService {
+	ChamadoDAO dao;
+	
+	public ChamadoService(){
+		dao = new ChamadoDAO();
+	}
+	
+	public int novoChamado(Chamado chamado) throws IOException{
+		chamado.setDataAbertura(new Date());
+		chamado.setDataFechamento(null);
+		chamado.setStatus(Chamado.ABERTO);
+		return -1;
+		//return dao.inserirChamado(chamado);
+	}
+	
+	public ArrayList<Chamado> listarChamados(Fila fila) throws IOException{
+		return dao.listarChamados(fila);
+	}
+
+}
